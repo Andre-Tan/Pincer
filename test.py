936f4b0e6d424c4da28d5045ac1af0b2658c4366
Andre-Tan <andre.sutanto.91@gmail.com>
2017-07-05 23:05:35 +1000

    Renamed

--- test.py
+++
@@ -1,8 +0,0 @@
-from primerO import PrimerO
-from primerO import Alignment
-from Bio.Seq import Seq
-from Bio import pairwise2
-
-primo = PrimerO("/home/andret1/reads/test.fasta")
-
-primo.run_primerO()
