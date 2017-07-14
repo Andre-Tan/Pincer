fceed7ed91bb2b61c82fe91887bfd535c8374974
Andre-Tan <andre.sutanto.91@gmail.com>
2017-07-14 15:04:30 +1000

    Updated tests

--- test_pincer.py
+++ test_pincer.py
@@ -7,13 +7,7 @@ test_no_path = "this is a null test"
 sequence_path = "/home/andret1/dev/Pincer/TestData/contig.fasta"
 seq_rc_path = "/home/andret1/dev/Pincer/TestData/contig_rev_comp.fasta"
 
-assembly_path_single = "/home/andret1/dev/Pincer/TestData/LD_10702_6_10.fa"
-assembly_path_multiple = "/home/andret1/reads/genomes_test/LD_SP5LAU.fa"
 primer_path = "/home/andret1/dev/Pincer/TestData/primer.fa"
-
-assembly_1 = "/home/andret1/reads/genomes_test/LD_19944_5_36.fa"
-assembly_2 = "/home/andret1/reads/genomes_test/LD_19944_5_65.fa"
-assembly_3 = "/home/andret1/reads/genomes_test/LD_19944_6_145.fa"
 
 default_pincer = generatePincerObj(query=sequence_path, primers=primer_path)
 test_pincer = generatePincerObj(query=assembly_2, primers=primer_path,
