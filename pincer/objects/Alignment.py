8e0007cc8236108a0ffd899c90ec50323fe3c4ba
Andre-Tan <andre.sutanto.91@gmail.com>
2017-08-02 13:38:27 +1000

    Now raise exception

--- pincer/objects/Alignment.py
+++ pincer/objects/Alignment.py
@@ -1,13 +1,20 @@
+import sys
+
+class wrongAlignmentLengthException(Exception):
+    pass
+    
 class Alignment:
     def __init__(self, align_tup):
-        assert len(align_tup) == 5, "Are you sure it is alignment output?"
+        if len(align_tup) != 5:
+            raise wrongAlignmentLengthException("Wrong formatting for Alignment!")
+            sys.exit(1)
+        
         subject, aligned, score, start, end = align_tup
         
         self.subject = subject
         self.aligned = aligned
         self.score = score
         self.Positions = (start, end)
-        
         
     def filter_by_score(self, score):
         return self.score >= score
